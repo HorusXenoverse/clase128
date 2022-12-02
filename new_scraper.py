9c5f2d4ceb706312e8bc7ffb6a14277924667aff
HorusXenoverse <92953478+HorusXenoverse@users.noreply.github.com>
2022-12-02 11:46:32 -0600

    Add files via upload

---
+++ new_scraper.py
@@ -0,0 +1,86 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
+import requests
+import time
+import pandas as pd
+
+# Enlace a NASA Exoplanet
+START_URL = "https://exoplanets.nasa.gov/exoplanet-catalog/"
+
+# Controlador web
+options = webdriver.EdgeOptions() 
+options.add_experimental_option('excludeSwitches', ['enable-logging']) 
+browser = webdriver.Edge(options=options)
+browser.get(START_URL)
+
+time.sleep(10)
+
+new_planets_data = []
+
+def scrape_more_data(hyperlink):
+    print(hyperlink)
+    ## AGREGA CÓDIGO AQUÍ ##
+    try:
+        page = requests.get(hyperlink)
+        soup = BeautifulSoup(page.content, "html.parser")
+        temporal_list = []
+
+        for scrape in soup.find_all("tr", attrs={"class": "fact_row"}):
+            td_save = scrape.find_all("td")
+
+            for add_temporal in td_save:
+                try:
+                    temporal_list.append(add_temporal.find_all("div", attrs={"class": "value"}) [0].contents[0] )
+        
+                except:
+                    temporal_list.append("") 
+        new_planets_data.append(temporal_list)
+
+
+    except:
+        time.sleep(1)
+        scrape_more_data(hyperlink)
+
+
+
+
+
+
+
+
+planet_df_1 = pd.read_csv("updated_scraped_data.csv")
+
+# Llamar al método
+for index, row in planet_df_1.iterrows():
+
+    ## ADGREGA CÓDIGO AQUÍ ##
+    print(row["hyperlink"])
+
+    # Llama a scrape_more_data(<hyperlink>)
+    scrape_more_data(row["hyperlink"])
+
+    print(f"La extracción de datos del hipervínculo {index+1} se ha completado")
+
+print(new_planets_data)
+
+# Remover el carácter '\n' de los datos extraídos
+scraped_data = []
+
+for row in new_planets_data:
+    replaced = []
+    ## AGREGAR EL CÓDIGO AQUÍ ##
+    for row_recorrer in row:
+        sin_linea_salto = row_recorrer.replace("\n", "")
+        replaced.append(sin_linea_salto)
+
+    scraped_data.append(replaced)
+
+print(scraped_data)
+
+headers = ["planet_type","discovery_date", "mass", "planet_radius", "orbital_radius", "orbital_period", "eccentricity", "detection_method"]
+
+new_planet_df_1 = pd.DataFrame(scraped_data,columns = headers)
+
+# Convertir a CSV
+new_planet_df_1.to_csv('new_scraped_data.csv', index=True, index_label="id")
